dd8ee02f56761f531caaf236f3b5c45dad27b804
RHood78 <r_hood78@yahoo.com>
2019-04-07 22:09:14 -0600

    install and service file changes

--- install_monero_miner.sh
+++ install_monero_miner.sh
@@ -75,6 +75,8 @@ sudo apt -y install libmicrohttpd-dev cmake libhwloc-dev
 #Download Latest Source of XMR-STAK miner
 git clone https://github.com/fireice-uk/xmr-stak.git
 
+sudo nano ~/monero_miner_deployment/xmr-stak/xmrstak/donate-level.hpp
+
 #Make build Directory
 mkdir xmr-stak/build
 
@@ -83,26 +85,38 @@ cd xmr-stak/build
 
 # Compile Miner
 cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+## add -DCUDA_ENABLE=OFF or -DOpenCL_ENABLE=OFF
+## or -DOpenCL_INCLUDE_DIR=/opt/amdgpu-pro/lib/x86_64-linux-gnu
+## or -DOpenCL_LIBRARY=/opt/AMDAPPSDK-3.0/lib/x86_64/sdk/libOpenCL.so -DOpenCL_INCLUDE_DIR=/opt/AMDAPPSDK-3.0/include/CL
+## or -DOpenCL_LIBRARY=/opt/amdgpu-pro/lib/x86_64-linux-gnu/libOpenCL.so -DOpenCL_INCLUDE_DIR=/opt/amdgpu-pro/bin
 
 # Install Miner
 make install
+## if u get error 'CL/cl.h: No such file or directory', try: sudo apt-get install opencl-headers
+
+#copy miner for systemd use
+cd bin
+cp bin/xmr-stak /usr/local/bin/xmr-stak # so system can execute
+#/usr/local/bin/xmr-stak -c ~/xmr-stak/config.txt
 
 # Change to Monero Miner Deployment Directory
-cd bin
+cd ~/monero_miner_deployment/
 
 # Copy Miner Service File
-sudo /bin/bash -c "cp $(pwd -P)/monero-miner.service /lib/systemd/system/"
+#sudo /bin/bash -c "cp $(pwd -P)/monero-miner.service /lib/systemd/system/"
+sudo /bin/bash -c "cp $(pwd -P)/monero-miner.service /etc/systemd/system/"
 
-# Update Miner Systemd Service File With Wallet & Pool Information
-sudo sed -i "s/walletaddress/$WALLET/" /lib/systemd/system/monero-miner.service
-sudo sed -i "s/mineaddress/$MINEHOST/" /lib/systemd/system/monero-miner.service
+## Update Miner Systemd Service File With Wallet & Pool Information
+#sudo sed -i "s/walletaddress/$WALLET/" /lib/systemd/system/monero-miner.service
+#sudo sed -i "s/mineaddress/$MINEHOST/" /lib/systemd/system/monero-miner.service
 
 # Reload Systemd Services
 sudo systemctl daemon-reload
 
-# Enable & Start the Miner
-sudo systemctl enable monero-miner.service
+## Enable & Start the Miner
 sudo systemctl start monero-miner.service
+sudo systemctl status monero-miner.service #chek for errors
+sudo systemctl enable monero-miner.service #enable for system startup
 
 # Install Complete
 echo "Install Complete!"
